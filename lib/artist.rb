a3fd997f5099aa3fe361704824f10c423a30d60e
Alison Golden <alisong779@yahoo.com>
2019-05-01 18:25:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,8 @@ require_relative '../lib/concerns/paramable'
 
 class Artist
   extend Memorable::ClassMethods 
-  extend Findable::ClassMethods 
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
